0f882a04243d6188e1b34461057ca44774f61945
Jonas Szadura <jonasmsz@hotmail.com>
2024-06-06 15:02:39 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -109,6 +109,34 @@ class Category extends Model {
 
     }
 
+    public function getProductsPage($page = 1, $itemsPerPage = 8)
+    {
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM tb_products a
+            INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+            INNER JOIN tb_categories c on c.idcategory = b.idcategory
+            WHERE c.idcategory = :idcategory
+            LIMIT $start, $itemsPerPage;
+        ", [
+            ':idcategory'=>$this->getidcategory()
+        ]);
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS mrtotal;");
+
+        return [
+            'data'=>Product::checklist($results),
+            'total'=>(int)$resultTotal[0]["mrtotal"],
+            'pages'=>ceil($resultTotal[0]["mrtotal"] / $itemsPerPage)
+        ];
+
+    }
+
     public function addProduct(Product $product)
     {
 
